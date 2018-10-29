cafecbe79969ce6cf9b4812d85272f9b0638377f
Jakob Olsson <32457895+JakobOlsson@users.noreply.github.com>
2018-10-29 22:20:42 +0100

    new jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -10,10 +10,26 @@ pipeline {
                 echo 'Hello world!';
                 echo 'Now we will build';
                 echo "Branhc ${BRANCH_NAME}"
+                script {
+                  test = BRANCH_NAME.split('/')
+                }
+                echo "TEST: ${test}"
                 /* script {
                     def scmVars = checkout(scm)
                     echo "${scmVars}"
                 } */
+                echo "Git Commit: ${GIT_COMMIT}"
+
+                script {
+                    if (params.GitCommit == "master") {
+                        println "if statement test"
+                        println "${GIT_COMMIT}"
+                        GitCommit = GIT_COMMIT
+                    }
+                }
+
+                echo "Git Commit Params: ${GitCommit}"
+                echo "Git Branch: ${GIT_BRANCH}"
             }
         }
         stage('Codebuild') {
@@ -26,10 +42,23 @@ pipeline {
                              credentialsType: 'jenkins',
                              projectName: 'jenkins-test', 
                              sourceControlType: 'project',
-                             sourceVersion: params.GitCommit,
+                             sourceVersion: GitCommit,
+                             envVariables: '[  { TEST, test} ]'
                              region: 'eu-central-1';
             }
             
+        }
+        stage("Deploy feature") {
+          when {
+              expression { test[0].toLowerCase() == "feature" }
+          }
+          steps {
+              script {
+                  name = test[1].toLowerCase()
+              }
+              echo "Deploy feature branch: ${name}"
+          }
+
         }
 
     }
--- scripts/deploy.py
+++ scripts/deploy.py
@@ -16,22 +16,29 @@ import boto3
 import botocore
 
 
-def get_keypair_names():
+def get_keypair_names(environment):
     """ Returns a list of EC2 KeyPair Names """
-    client = boto3.client('ec2')
+    cfg = get_config(environment)
+    reg = cfg.get('region')
+    client = boto3.client('ec2', region_name=reg)
     keypairs = client.describe_key_pairs().get('KeyPairs')
     if keypairs:
         return [x['KeyName'] for x in keypairs]
     return []
 
 
-def create_keypair(name):
+def create_keypair(environment, name):
     """ Create an EC2 KeyPair with name - if it doesn't already exist """
-    if name in get_keypair_names():
+    cfg = get_config(environment)
+    reg = cfg.get('region')
+    kpn = cfg.get('keypair_name')
+    if kpn == '':
+        print('ERROR: No keypair name configured')
+    if name in get_keypair_names(environment):
         print("Key:", name, "already exists")
     else:
         print("Creating key:", name)
-        client = boto3.client('ec2')
+        client = boto3.client('ec2',region_name=reg)
         ekp = client.create_key_pair(KeyName=name)
         print("Created:", ekp.get('KeyName'))
         print("PrivateKey:\n", ekp.get('KeyMaterial', 'missing'))
@@ -65,9 +72,11 @@ def create_bucket(name):
         print("Created:", stb.get('Location', 'missing'))
 
 
-def get_stack_names():
+def get_stack_names(environment):
     """ Returns a list of CloudFormation Stack names """
-    client = boto3.client('cloudformation')
+    cfg = get_config(environment)
+    reg = cfg.get('region')
+    client = boto3.client('cloudformation', region_name=reg)
     stacks = client.list_stacks().get('StackSummaries', [])
     stacknames = []
     for stack in stacks:
@@ -76,9 +85,11 @@ def get_stack_names():
     return stacknames
 
 
-def get_stack_info(name):
+def get_stack_info(environment, name):
     """ Get a dictonary object with detailed info for stack with name """
-    client = boto3.client('cloudformation')
+    cfg = get_config(environment)
+    reg = cfg.get('region')
+    client = boto3.client('cloudformation', region_name=reg)
     stackinfo = client.describe_stacks(StackName=name).get('Stacks')
     if stackinfo is not None:
         return stackinfo[0]
@@ -128,17 +139,17 @@ def get_status(environmentname):
     stts['region'] = cnfg['region']
     stts['keypair'] = {'name': cnfg['keypair_name'],
                        'status':
-                       cnfg['keypair_name'] in get_keypair_names()}
+                       cnfg['keypair_name'] in get_keypair_names(environmentname)}
     stts['bucket_name'] = {'name': cnfg['bucket_name'],
                            'status':
                            cnfg['bucket_name'] in get_s3bucket_names()}
 
     stts['stacks'] = []
-    stacks = get_stack_names()
+    stacks = get_stack_names(environmentname)
     for stack in stacks:
         if environmentname not in stack:
             continue
-        info = get_stack_info(stack)
+        info = get_stack_info(environmentname, stack)
         stts['stacks'].append({'name':
                                stack,
                                'satus':
@@ -153,12 +164,12 @@ def get_status(environmentname):
 def get_deployed():
     """ Returns a list of deployed environments """
     dpld = []
-    stacks = get_stack_names()
+    stacks = get_stack_names('DEFAULT')
     for stack in stacks:
         # Don't check tangled stacks
         if '-' in stack:
             continue
-        info = get_stack_info(stack)
+        info = get_stack_info('DEFAULT', stack)
         tags = info.get('Tags')
         for tagpair in tags:
             if 'environment' in tagpair.get('Key') \
@@ -244,12 +255,13 @@ def deploy(environment):
     ver = cfg.get('version')
     s3b = cfg.get('bucket_name')
     kpn = cfg.get('keypair_name')
+    reg = cfg.get('region')
     if ver != '' \
             and s3b in get_s3bucket_names() \
-            and kpn in get_keypair_names():
+            and kpn in get_keypair_names(environment):
         print("We have config")
         upload(environment)
-        client = boto3.client('cloudformation')
+        client = boto3.client('cloudformation', region_name=reg)
         bas_tpl = "https://" + s3b + "s3.amazonaws.com/"
 
     else:
@@ -280,21 +292,19 @@ def main():
               "\n", "It reads it's configuration form deploy.conf",
               "\n\n", "Options",
               "\n", " --help, -h\tPrints this help message"
-              "\n", " --list-keys\tList existing EC2 Key Pair Names"
-              "\n", " --create-key name\tCreate a EC2 Key Pair"
+              "\n", " --list-keys environment\tList existing EC2 Key Pair Names for environment"
+              "\n", " --create-key environment\tCreate a EC2 Key Pair"
               "\n", " --list-buckets\tList existing s3 buckets"
               "\n", " --create-bucket name\tCreate a s3 bucket"
               "\n", " --upload\tUpload cloudformation scripts to s3 bucket"
-              "\n", " --status name\tStatus for given environment"
+              "\n", " --status environment\tStatus for given environment"
               "\n", " --list-deployed\tlist deployed environments"
-              "\n", " --deploy name\tDeploy/Update given environment")
+              "\n", " --deploy environmentname\tDeploy/Update given environment")
 
     # Single option with no value
     # Or option requiring a missing value
     elif len(sys.argv) == 2:
-        if "list-keys" in sys.argv[1]:
-            print("Key Names:", ", ".join(get_keypair_names()))
-        elif "list-buckets" in sys.argv[1]:
+        if "list-buckets" in sys.argv[1]:
             print("Bucket Names:", ", ".join(get_s3bucket_names()))
         elif "list-deployed" in sys.argv[1]:
             prpr.pprint(get_deployed())
@@ -307,6 +317,8 @@ def main():
     else:
         if "create-key" in sys.argv[1]:
             create_keypair(sys.argv[2])
+        if "list-keys" in sys.argv[1]:
+            print("Key Names:", ", ".join(get_keypair_names(sys.argv[2])))
         elif "create-bucket" in sys.argv[1]:
             create_bucket(sys.argv[2])
         elif "status" in sys.argv[1]:
